461abd1a12a923889e9ecc5ad2cd776a47e9cfc6
argonteqarslan <rana.arslan@argonteq.com>
2022-05-17 15:59:38 +0500

    deck routes

--- src/routes/deckRoutes.js
+++ src/routes/deckRoutes.js
@@ -1,6 +1,5 @@
-import express from "express"
+import express from "express";
 import deckController from "../controllers/deckController.js";
-
 
 import multer from "multer";
 
@@ -18,27 +17,36 @@ let uploadFile = multer({
   storage: storage,
 });
 const userProfileRouter = express.Router();
-import {authJwt} from "../middlewares/authJwt.js";
-
+import { authJwt } from "../middlewares/authJwt.js";
 
 // create a new deck
 
-userProfileRouter.route("/create").post(authJwt, uploadFile.single("profileImg"),deckController.createDeck);
+userProfileRouter
+  .route("/create")
+  .post(authJwt, uploadFile.single("profileImg"), deckController.createDeck);
 
 // add a card to the deck
 
-userProfileRouter.route("/:deckId/add-card").post(authJwt, uploadFile.single("profileImg"),deckController.addCardToDeck);
+userProfileRouter
+  .route("/:deckId/add-card")
+  .post(authJwt, uploadFile.single("profileImg"), deckController.addCardToDeck);
 
 // get all the cards of the deck
 
-userProfileRouter.route("/:deckId/all-cards").get(authJwt,deckController.getAllCardsOfDeck);
+userProfileRouter
+  .route("/:deckId/all-cards")
+  .get(authJwt, deckController.getAllCardsOfDeck);
 
 // remove card from the deck
 
-userProfileRouter.route("/:deckId/card/:cardId/delete").delete(authJwt,deckController.deleteCardById);
+userProfileRouter
+  .route("/:deckId/card/:cardId/delete")
+  .delete(authJwt, deckController.deleteCardById);
 
 // get a card from the deck
 
-userProfileRouter.route("/:deckId/card/:cardId").get(authJwt,deckController.getCardById);
+userProfileRouter
+  .route("/:deckId/card/:cardId")
+  .get(authJwt, deckController.getCardById);
 
-export default userProfileRouter
+export default userProfileRouter;
