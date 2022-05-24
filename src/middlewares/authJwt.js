b8463410b2edf4ae29b61da2968e38af2f1f39e3
argonteqarslan <rana.arslan@argonteq.com>
2022-05-24 19:16:14 +0500

    fixed auth jwt

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -2,7 +2,12 @@ import {User} from "../models/users.js"
 import jwt_decode from "jwt-decode";
 
 let authJwt = async (req, res, next) => {
-  var token = req.headers.authorization.replace("Bearer ", "");
+  var token = "";
+  if (req.headers.authorization) {
+    token = req.headers.authorization.replace("Bearer ", "");
+  } else {
+    token = req.headers.token
+  }
   var decod = jwt_decode(token);
   let name = decod.username;
   // console.log(decoded);
@@ -10,15 +15,15 @@ let authJwt = async (req, res, next) => {
     where: {
       username: name,
     },
-  })
+  });
   if (!user) {
-      res.send({
-          "code": 400,
-          "error": "Invalid JWT"
-      })
+    res.send({
+      code: 400,
+      error: "Invalid JWT",
+    });
   } else {
-    req.user = user
-    next()
+    req.user = user;
+    next();
   }
 };
 
