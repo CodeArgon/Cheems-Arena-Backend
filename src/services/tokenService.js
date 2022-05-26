c7f62574d1e009a120a9192a7cc94fb209d00548
argonteqarslan <rana.arslan@argonteq.com>
2022-05-26 13:12:31 +0500

    token service changes

--- src/services/tokenService.js
+++ src/services/tokenService.js
@@ -1,16 +1,16 @@
-import fetch from 'node-fetch'
-import { Metadata } from '@metaplex-foundation/mpl-token-metadata'
-import { PublicKey } from '@solana/web3.js'
-import { TOKEN_PROGRAM_ID } from '@solana/spl-token'
-import con from '../config/db.js'
-import { MintList } from '../models/mint_list.js'
+import fetch from "node-fetch";
+import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
+import { PublicKey } from "@solana/web3.js";
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import con from "../config/db.js";
+import { MintList } from "../models/mint_list.js";
 
-let addresses=[];
-const tokenService={
+let addresses = [];
+const tokenService = {
   getTokenAccounts: async (connection, pubKeyString) => {
     let accounts;
     if (pubKeyString) {
-      accounts=await connection.getParsedProgramAccounts(TOKEN_PROGRAM_ID, {
+      accounts = await connection.getParsedProgramAccounts(TOKEN_PROGRAM_ID, {
         filters: [
           {
             dataSize: 165,
@@ -22,49 +22,48 @@ const tokenService={
             },
           },
         ],
-      })
+      });
     }
 
     console.log(
       `Found ${accounts?.length} account(s) for pubKey ${pubKeyString}.`
-    )
-    return accounts
+    );
+    return accounts;
   },
   getTokenData: async (connection, token) => {
     try {
-      if (token&&token.mint) {
-
-        let mintPubkey=new PublicKey(token.mint)
-        let tokenmetaPubkey=await Metadata.getPDA(mintPubkey)
-        let tokenData=await Metadata.load(connection, tokenmetaPubkey)
+      if (token && token.mint) {
+        let mintPubkey = new PublicKey(token.mint);
+        let tokenmetaPubkey = await Metadata.getPDA(mintPubkey);
+        let tokenData = await Metadata.load(connection, tokenmetaPubkey);
         const addresses = await MintList.findAll();
-        for (let i=1; i<addresses.length; i++) {
+        for (let i = 1; i < addresses.length; i++) {
           if (addresses[i].mint === tokenData.data.mint) {
-            return tokenData
+            return tokenData;
           }
         }
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   },
   getMetaData: async (tokenData) => {
-    let metaData={}
+    let metaData = {};
     if (tokenData) {
-      let metaDataUri=tokenData.data?.data?.uri
+      let metaDataUri = tokenData.data?.data?.uri;
 
-      let response=await fetch(metaDataUri)
+      let response = await fetch(metaDataUri)
         .then((res) => res.json())
         .catch((err) => {
-          console.log(err)
-        })
+          console.log(err);
+        });
 
-      if (response&&response.image) {
-        metaData=response
+      if (response && response.image) {
+        metaData = response;
       }
     }
-    return metaData
+    return metaData;
   },
-}
+};
 
-export default tokenService
+export default tokenService;
