e46ec823de3785b6d231372cf0306c8bde50ab5d
argonteqarslan <rana.arslan@argonteq.com>
2022-06-08 21:07:36 +0500

    profile img added

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -6,97 +6,105 @@ import { User } from "../models/users.js";
 import { CardModel } from "../models/cardModels.js";
 
 const apiController = {
-    getTokens: async (req, res) => {
-        let username = req.body.username;
-        let password = req.body.password;
+  getTokens: async (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
 
-        const user = await User.findOne({
-            where: {
-                username
-            }
-        })
-        if (!user) {
-            res.status(400).send({
-                "code": 400,
-                "error": "Username or password are incorrect"
-            })
-        } else {
-            let pubKey = user.walletAddress
+    const user = await User.findOne({
+      where: {
+        username
+      }
+    })
 
-            // compare passwords [ from db and users input ]
-            let comparison = await bcrypt.compare(password, user.password).then()
-            if (comparison) {
-                if (!pubKey) {
-                    return res.status(400).json({ message: "Error. Please connect wallet and send pubKey to verify." });
-                }
+    if (!user) {
+      res.status(400).send({
+        "code": 400,
+        "error": "Username or password are incorrect"
+      })
 
-                const payload = {
-                    username,
-                    password
-                };
+    } else {
+      let pubKey = user.walletAddress
 
-                const token = jwt.sign(
-                    payload,
-                    "thisissecretkey",
-                    { expiresIn: '1 day' },
-                );
-                // get metadata against public key
-                // let metaDataArray = await tokenController.getMetaData(pubKey);
-                let cards = await CardModel.findAll();
+      // compare passwords [ from db and users input ]
+      let comparison = await bcrypt.compare(password, user.password).then()
 
-                let metaDataArray = cards.map(function (currentValue, Index) {
-                  currentValue.dataValues.INDEX = Index + 1
-                  currentValue.dataValues.attributes = [
-                    {
-                      "trait_type": "Mana",
-                      "value": currentValue.dataValues.mana
-                    },
-                    {
-                      "trait_type": "Attack",
-                      "value": currentValue.dataValues.attack
-                    },
-                    {
-                      "trait_type": "Hp",
-                      "value": currentValue.dataValues.hp
-                    },
-                    {
-                      "trait_type": "Specification",
-                      "value": currentValue.dataValues.specification
-                    },
-                    {
-                      "trait_type": "Rarity",
-                      "value": currentValue.dataValues.rarity
-                    }
-                  ]
-                  currentValue.dataValues.collection = {
-                    "name": "Cheems Arena",
-                    "family": "Cheems"
-                  }
-                  currentValue.dataValues.seller_fee_basis_points = 400
-                  return currentValue
-                })
-          
-                let response = {
-                  LoginDataRoot: {
-                    pubKey,
-                    data: metaDataArray,
-                    message: "Token metaData found in user wallet.",
-                    token,
-                    username: user.username,
-                    email: user.email,
-                    walletAddress: user.walletAddress,
-                  },
-                };
-                res.json(response);
-            } else {
-                res.status(400).send({
-                    "code": 400,
-                    "error": "Username or password doesn't match"
-                })
-            }
+      if (comparison) {
+        if (!pubKey) {
+          return res.status(400).json({ message: "Error. Please connect wallet and send pubKey to verify." });
         }
 
+        const payload = {
+          username,
+          password
+        };
+
+        const token = jwt.sign(
+          payload,
+          "thisissecretkey",
+          { expiresIn: '1 day' },
+        );
+        // get metadata against public key
+        // let metaDataArray = await tokenController.getMetaData(pubKey);
+        let cards = await CardModel.findAll();
+
+        let metaDataArray = cards.map(function (currentValue, Index) {
+
+          currentValue.dataValues.INDEX = Index + 1
+
+          currentValue.dataValues.attributes = [
+            {
+              "trait_type": "Mana",
+              "value": currentValue.dataValues.mana
+            },
+            {
+              "trait_type": "Attack",
+              "value": currentValue.dataValues.attack
+            },
+            {
+              "trait_type": "Hp",
+              "value": currentValue.dataValues.hp
+            },
+            {
+              "trait_type": "Specification",
+              "value": currentValue.dataValues.specification
+            },
+            {
+              "trait_type": "Rarity",
+              "value": currentValue.dataValues.rarity
+            }
+          ]
+
+          currentValue.dataValues.collection = {
+            "name": "Cheems Arena",
+            "family": "Cheems"
+          }
+          
+          currentValue.dataValues.seller_fee_basis_points = 400
+          return currentValue
+        })
+
+        let response = {
+          LoginDataRoot: {
+            pubKey,
+            data: metaDataArray,
+            message: "Token metaData found in user wallet.",
+            token,
+            username: user.username,
+            email: user.email,
+            walletAddress: user.walletAddress,
+            profileImg: user.profileImg,
+          },
+        };
+        res.json(response);
+      } else {
+        res.status(400).send({
+          "code": 400,
+          "error": "Username or password doesn't match"
+        })
+      }
     }
+
+  }
 }
 
 export default apiController;
